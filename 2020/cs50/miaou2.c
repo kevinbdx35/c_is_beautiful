49eebf9df1f9f74e40edb3bc7b7f72336d2e7239
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2022-12-12 18:53:03 +0100

    Create miaou2.c

---
+++ 2020/cs50/miaou2.c
@@ -0,0 +1,13 @@
+//solution sans fonction et avec une boucle for
+#include <cs50.h>
+#include <stdio.h>
+
+// Better design
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        printf("miaou\n");
+    }
+}
