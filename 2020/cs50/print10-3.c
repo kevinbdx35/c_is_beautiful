3996676024f9befbbca31d62bc8865b7e75d16ed
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2022-12-12 19:02:57 +0100

    Create print10-3.c

---
+++ 2020/cs50/print10-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++) //suppression du symbol "="
+	 {
+		  printf("i vaut maintenant %i: ", i); //ceci n'est pas le but du programme, mais juste un diagnostique temporaire pour nous aider à comprendre ce qui se passe
+			printf("#\n");
+	 }
+}
