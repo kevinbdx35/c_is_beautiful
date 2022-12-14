e488ddd49a89d10e324addb73a59edfdbe2ea928
Kévin <56440487+kevinbdx35@users.noreply.github.com>
2022-12-14 22:13:55 +0100

    Create numbers.c

---
+++ 2020/cs50/numbers.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int numbers[6];
+	numbers[0] = 4;
+	numbers[1] = 8;
+	numbers[2] = 15;
+	numbers[3] = 16;
+	numbers[4] = 23;
+	numbers[5] = 42;
+...
+}
